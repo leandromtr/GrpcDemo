6cc5852ec54cb81bfdbbcffdcb88f8a7ff7872f2
LeandroReis <leandromtr@hotmail.com>
2021-07-06 08:04:05 +0200

    implement first method

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var input = new HelloRequest { Name = "Leandro" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
             Console.ReadLine();
         }
     }
